30e6b3266b8dd71002e632080cfe4e409bb9c0ae
ShirishaKb <shirishakb@gmail.com>
2024-10-24 18:12:21 -0400

    modified schema and seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,34 @@
-use employee_db
--- Insert data into the employees table
+-- Connect to the database
+\c employees
 
-INSERT INTO department (name)
-VALUES ('Engineering'),
-       ('Sales'),
-       ('Finance'),
-       ('Legal');
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ('Lead Engineer', 100000, 1),
-       ('Engineer', 80000, 1),
-       ('Sales Lead', 80000, 2),
-       ('Salesperson', 60000, 2),
-       ('Accountant', 70000, 3),
-       ('Legal Team Lead', 250000, 4),
-       ('Lawyer', 190000, 4);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, NULL),
-       ('Mike', 'Chan', 2, 1),
-       ('Ashley', 'Rodriguez', 3, NULL),
-       ('Kevin', 'Tupik', 4, 3),
-       ('Malia', 'Brown', 5, 3),
-       ('Sarah', 'Lourd', 6, NULL),
-       ('Tom', 'Allen', 7, 6);
-       
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
